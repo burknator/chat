d6aaf54673716890bc4d035b04ac6222705e73a4
Patrick Burke <hi@pburke.de>
2021-02-28 12:23:00 +0100

    prototype views and tailwind installation

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,10 +2,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
+        return req.view.render("sub/index")
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("room", ":id") { req in
+        return req.view.render("sub/chat-room")
     }
 }
